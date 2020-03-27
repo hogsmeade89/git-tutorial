50b187072b28494709a28992ea6530a36a9cd989
Caohagan <60864139+hogsmeade89@users.noreply.github.com>
2020-03-27 19:06:51 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,21 @@
+import tensorflow as tf
+
+from .layers import Layer, Dense
+from .inits import glorot, zeros
+
+class MeanAggregator(Layer):
+    """
+    Aggregates via mean followed by matmul and non-linearity.
+    """
+
+    def __init__(self, input_dim, output_dim, neigh_input_dim=None,
+            dropout=0., bias=False, act=tf.nn.relu, 
+            name=None, concat=False, **kwargs):
+        super(MeanAggregator, self).__init__(**kwargs)
+
+        self.dropout = dropout
+        self.bias = bias
+        self.act = act
+        self.concat = concat
+
+        if neigh_input_dim is None:
